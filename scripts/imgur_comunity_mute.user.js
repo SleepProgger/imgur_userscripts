6ed2a5a0ee3f87d8bb9194655248f3e7c0174d84
Austin <tinoesroho@gmail.com>
2017-09-10 16:49:15 -0600

    onscroll firing + confirmation on click
    Added an onscroll listener. Tested and working.
    Added confirmation prompt to avoid accidental mutings.
    Improved muting: completely hide muted user's name and avatar in header.

--- scripts/imgur_comunity_mute.user.js
+++ scripts/imgur_comunity_mute.user.js
@@ -2,15 +2,22 @@
 // @name        imgur_comunity_mute
 // @namespace   someName
 // @include     https://community.imgur.com/*
-// @version     0.41a
+// @version     0.5a
 // @grant       none
 // ==/UserScript==
-
-
 /*
 * Patch for GM_getValue and GM_SetValue support for chrome
 * credits to: www.devign.me/greasemonkey-gm_getvaluegm_setvalue-functions-for-google-chrome/
 */
+var run_script = false;
+
+window.onload = (function(){
+    if (window.location.href.indexOf("/t/") > 0) {
+        run_script = true;
+        MakeMagic();
+    }
+});    
+
 if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf("not supported")>-1)) {
     this.GM_getValue=function (key,def) {
         return localStorage[key] || def;
@@ -23,35 +30,59 @@ if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString
     };
 }
 
+var GR_COOKIE_NAME = 'discourse_mutefile';
+var hide_ids = $.parseJSON(GM_getValue(GR_COOKIE_NAME, '{}'));
 
-$('body').ready(function(){
-	var GR_COOKIE_NAME = 'imgur_community_mute';
-	var hide_ids = $.parseJSON(GM_getValue(GR_COOKIE_NAME, '{}'));
+document.addEventListener("wheel", function(event) { 
+    setTimeout(MakeMagic, 1000);
+});
 
+function RecastSpell(){
+    if (run_script == true) {
+        MakeMagic();
+    }
+}
+
+function MakeMagic(){
+    hide_ids = $.parseJSON(GM_getValue(GR_COOKIE_NAME, '{}'));
 	function handle_post_node(node){
 		var tid = node.getAttribute('data-user-id');
+		var name = $('[data-user-id="'+tid+'"]').find('.username a').first().text();
 		function mute_foo(){
-			this.innerHTML = "Unmute";
+		    //var name = $(node).find('.names').text;
+		    var confirmString = "Really mute " + name + "?";  
+		    if (confirm(confirmString) == true) {
+                        this.innerHTML = "Unmute;"
 			$(node).find('.contents').hide();
+			$(node).find('.names').hide();
+			$(node).find('.avatar').hide();
 			$(this).unbind('click', mute_foo);
 			$(this).click(umute_foo);
 			hide_ids[tid] = 1;
 			GM_setValue(GR_COOKIE_NAME, JSON.stringify(hide_ids));
 			$('[data-user-id="'+tid+'"]').find('.mute_btn').remove();
-			$('[data-user-id="'+tid+'"]').each(function(){ handle_post_node(this) });
+			$('[data-user-id="'+tid+'"]').each(function(){ handle_post_node(this) });          
+		    }
 		}
 		function umute_foo(){
+		    var confirmString = "Unmute " + name + "?";  
+		    if (confirm(confirmString) == true) {
 			this.innerHTML = "Mute";
 			$(node).find('.contents').show();
+			$(node).find('.names').show();
+			$(node).find('.avatar').show();
 			$(this).unbind('click', umute_foo);
 			$(this).click(mute_foo);
 			delete hide_ids[tid];
 			GM_setValue(GR_COOKIE_NAME, JSON.stringify(hide_ids));
 			$('[data-user-id="'+tid+'"]').find('.umute_btn').remove();
 			$('[data-user-id="'+tid+'"]').each(function(){ $(this).find('.contents').show(); handle_post_node(this) });
+		    }
 		}
 		if(hide_ids[tid]){			
 			$(node).find('.contents').hide();
+			$(node).find('.names').hide();
+			$(node).find('.avatar').hide();
 			if($(node).find('.umute_btn').length > 0) return;
 			var btn = $('<button class="umute_btn" title="Unmute this user." style="background-color: Transparent; background-repeat:no-repeat; border: none; cursor:pointer; overflow: hidden; outline:none; margin-left: 3px; "><i class="fa fa-microphone"></i></button>');
 			$(node).find('.post-info').first().prepend(btn);
@@ -92,4 +123,4 @@ $('body').ready(function(){
 		}
 	});
 	observer.observe(document, { subtree: true, childList: true});
-});
+}
