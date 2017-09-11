610aeb3a108a0234f0507e81e545d451ed497934
Austin <tinoesroho@gmail.com>
2017-09-10 23:01:04 -0600

    Update imgur_comunity_mute.user.js
    confirmed working for chrome, firefox

--- scripts/imgur_comunity_mute.user.js
+++ scripts/imgur_comunity_mute.user.js
@@ -1,23 +1,16 @@
 // ==UserScript==
-// @name        imgur_comunity_mute
+// @name        imgur_community_mute
 // @namespace   someName
 // @include     https://community.imgur.com/*
 // @version     0.5a
-// @grant       GM_addStyle
+// @grant       none
 // ==/UserScript==
+$('head').append('<link rel="stylesheet" href="https://gitcdn.xyz/repo/tinoesroho/discourse_repo/master/scripts/mute.css" type="text/css" />');
+
 /*
 * Patch for GM_getValue and GM_SetValue support for chrome
 * credits to: www.devign.me/greasemonkey-gm_getvaluegm_setvalue-functions-for-google-chrome/
 */
-var run_script = false;
-GM_addStyle("@keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-moz-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-webkit-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-ms-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}@-o-keyframes nodeInserted{from{outline-color:#fff}to{outline-color:#000}}#article,.article,article{animation-duration:0.01s;-o-animation-duration:0.01s;-ms-animation-duration:0.01s;-moz-animation-duration:0.01s;-webkit-animation-duration:0.01s;animation-name:nodeInserted;-o-animation-name:nodeInserted;-ms-animation-name:nodeInserted;-moz-animation-name:nodeInserted;-webkit-animation-name:nodeInserted}");
-$(document).ready(function(){
-    if (window.location.href.indexOf("/t/") > 0) {
-        run_script = true;
-        MakeMagic();
-    }
-});    
-
 if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString().indexOf("not supported")>-1)) {
     this.GM_getValue=function (key,def) {
         return localStorage[key] || def;
@@ -30,13 +23,15 @@ if (!this.GM_getValue || (this.GM_getValue.toString && this.GM_getValue.toString
     };
 }
 
-var GR_COOKIE_NAME = 'imgur_community_mute';
-var hide_ids = $.parseJSON(GM_getValue(GR_COOKIE_NAME, '{}'));
+
+$('body').ready(function(){
+  doWork();
+});
 
 (function(){
     event = function(event){
         if (event.animationName == 'nodeInserted') {
-            RecastSpell();
+            doWork();
         }
     }
         
@@ -45,70 +40,41 @@ document.addEventListener('MSAnimationStart', event, false);
 document.addEventListener('webkitAnimationStart', event, false);
 })();
 
-function RecastSpell(){
-    if (run_script == true) {
-        MakeMagic();
-    }
-}
-
-function MakeMagic(){
-    hide_ids = $.parseJSON(GM_getValue(GR_COOKIE_NAME, '{}'));
+	var GR_COOKIE_NAME = 'imgur_community_mute';
+	var hide_ids = $.parseJSON(GM_getValue(GR_COOKIE_NAME, '{}'));
+function doWork() {
 	function handle_post_node(node){
 		var tid = node.getAttribute('data-user-id');
-		var name = $('[data-user-id="'+tid+'"]').find('.username a').first().text();
 		function mute_foo(){
-		    //var name = $(node).find('.names').text;
-		    var confirmString = "Really mute " + name + "?";  
-		    if (confirm(confirmString) == true) {
-                serveJustice();
-                this.innerHTML = "Unmute;"
+			this.innerHTML = "Unmute";
 			$(node).find('.contents').hide();
-			$(node).find('.names').hide();
-			$(node).find('.avatar').hide();
 			$(this).unbind('click', mute_foo);
 			$(this).click(umute_foo);
 			hide_ids[tid] = 1;
 			GM_setValue(GR_COOKIE_NAME, JSON.stringify(hide_ids));
 			$('[data-user-id="'+tid+'"]').find('.mute_btn').remove();
 			$('[data-user-id="'+tid+'"]').each(function(){ handle_post_node(this) });
-		        setTimeout(endAnimation, 3000);            
-		    }
-		}
-
-		function serveJustice() {
-		    var overlay = jQuery('<div id="overlay" class="overlay"> </div>');
-            overlay.appendTo(document.body);
-		}
-		function endAnimation(){
-		 	$( ".overlay" ).remove();
 		}
 		function umute_foo(){
-		    var confirmString = "Unmute " + name + "?";  
-		    if (confirm(confirmString) == true) {
 			this.innerHTML = "Mute";
 			$(node).find('.contents').show();
-			$(node).find('.names').show();
-			$(node).find('.avatar').show();
 			$(this).unbind('click', umute_foo);
 			$(this).click(mute_foo);
 			delete hide_ids[tid];
 			GM_setValue(GR_COOKIE_NAME, JSON.stringify(hide_ids));
 			$('[data-user-id="'+tid+'"]').find('.umute_btn').remove();
 			$('[data-user-id="'+tid+'"]').each(function(){ $(this).find('.contents').show(); handle_post_node(this) });
-		    }
 		}
 		if(hide_ids[tid]){			
 			$(node).find('.contents').hide();
-			$(node).find('.names').hide();
-			$(node).find('.avatar').hide();
 			if($(node).find('.umute_btn').length > 0) return;
 			var btn = $('<button class="umute_btn" title="Unmute this user." style="background-color: Transparent; background-repeat:no-repeat; border: none; cursor:pointer; overflow: hidden; outline:none; margin-left: 3px; "><i class="fa fa-microphone"></i></button>');
-			$(node).find('.post-info').first().append(btn);
+			$(node).find('.post-info').first().prepend(btn);
 			btn.click(umute_foo);
 		}else {
 			if($(node).find('.mute_btn').length > 0) return;
 			var btn = $('<button title="Mute this user." style="background: none;" class="mute_btn"><i class="fa fa-microphone-slash"></i></button>');
-			$(node).find('.post-info').first().append(btn);
+			btn.insertBefore($(node).find('.create').last());
 			btn.click(mute_foo);
 		}
 	}
